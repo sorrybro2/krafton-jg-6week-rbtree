435dbd881e799b7ad4a0724e6b23143227e36854
IM_SORRY <136697975+sorrybro2@users.noreply.github.com>
2025-08-15 21:17:40 +0900

    delete_rbtree() 함수 구현

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -19,13 +19,22 @@ rbtree *new_rbtree(void) {
   return p;
 }
 
+// rbtree에 할당한 모든 메모리를 해체한다.
 void delete_rbtree(rbtree *t) {
-  free(t->nil);
+  // 재귀 중에 t->nil을 계속 참조하므로 노드들부터 메모리 해제
+  delete_rbtree_recursive(t, t->root);
+
+  //new_rbtree()함수에서 각각 calloc으로 할당해줬으므로, 각각 t->nil, t를 free
+  free(t->nil); 
   free(t);
 }
 
+// delete_rbtree 함수가 실행되면 안전하게 후위로 노드 메모리 해제
 void delete_rbtree_recursive(rbtree *t, node_t *node){
-  
+  if(node==t->nil) return;
+  delete_rbtree_recursive(t, node->left);
+  delete_rbtree_recursive(t, node->right);
+  free(node);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
